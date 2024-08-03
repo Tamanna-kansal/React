bca5775f5922acda07f591ae7353ad5bb664c181
Tamanna kansal <tamannakansal118@gmail.com>
2024-08-03 11:54:40 +0530

    start commiting

---
+++ project-1/src/components/Hero.jsx
@@ -0,0 +1,24 @@
+const Hero=()=>{
+    return(
+        <>
+        <main className="hero container" >
+            <div className="hero-content">
+                <h2>YOUR FEET DESERE THE BEST</h2>
+                <p> AND WE ARE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WERE HERE TO HELP YOU WITH OUR SHOES.</p>
+                <div className="hero-btn">
+                <button>Shop Now</button>
+                <button className="secondary-btn">Category</button>
+                </div>
+                <div className="shopping">
+                    <p>Also Available on </p>
+                </div>
+            </div>
+            <div className="hero-image">
+                <img src="https://media.istockphoto.com/id/171370292/photo/nike-flyknit-trainer.jpg?s=612x612&w=is&k=20&c=vdFQCi4A3wucjjsevspoITfYKlkeJ7blybFoFqwmdEw="></img>
+            </div>
+        </main>
+        </>
+    )
+}
+
+export default Hero;
---
+++ project-1/src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+const Navigation=()=>{
+    return(
+        <>
+         <nav className="container">
+        <div className="logo">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/9/94/Old_Nike_logo.jpg" alt="logo"></img>
+        </div>
+        <ul type="none">
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+        </>
+    )
+}
+
+export default Navigation;
